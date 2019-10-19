68c62706c19e2094bfad8eaede214e1d640060c9
PiotrusWatson <piotruswatson@gmail.com>
2019-10-19 12:42:13 +0100

    made first pass at parsing messages from server

--- Bots/overlord.py
+++ Bots/overlord.py
@@ -4,54 +4,68 @@ from ServerMessageTypes import ServerMessageTypes
 from ServerComms import ServerComms
 import threading
 import time
-import argparse
 import logging
-import multiprocessing
-
-class MultiProcessingTank(multiprocessing.Process):
-	
-	def __init__(self, name, port=8052, hostname='127.0.0.1'):
-		multiprocessing.Process.__init__(self)
-		
-		self.server = ServerComms(hostname, port)
-		self.name = name
-		logging.info("Creating tank with name '{}'".format(name))
-	
-	def run(self):
-		self.server.sendMessage(ServerMessageTypes.CREATETANK, {'Name': self.name, })
-		while True:
-		
-			message = self.server.readMessage()
-			#logging.info(message)
-		return
 
 class ThreadingTank(threading.Thread):
 	
 	def __init__(self, name, port=8052, hostname='127.0.0.1'):
 		threading.Thread.__init__(self)
-		
+		self.ids_to_messages = {}
+		self.items_to_ids = {"Tank":[], 
+		"HealthPickup": [], 
+		"AmmoPickup": [],
+		"SnitchPickup": []}
+		self.status = {}
 		self.server = ServerComms(hostname, port)
 		self.name = name
 		logging.info("Creating tank with name '{}'".format(name))
 	
+	
+	
+	"""
+	Sends any message given to the server :)
+	"""
+	def messageServer(self, newMessage, params = {}):
+		self.server.sendMessage(newMessage, params )
+		#logging.info("Attempted to " + ServerMessageTypes.toString(newMessage))
+	
+	def getItems(self, message):
+		if not "Id" in message:
+			return
+			
+		id = message["Id"]
+		type = message["Type"]
+		self.ids_to_messages[id] = message
+		if (id not in self.items_to_ids[type]):
+			self.items_to_ids[type].append(id)
+			
 	def run(self):
 		self.server.sendMessage(ServerMessageTypes.CREATETANK, {'Name': self.name, })
 		while True:
-		
-			message = self.server.readMessage()
-			#logging.info(message)
+			self.message = self.server.readMessage()
+			#logging.info(self.message)
+			self.getItems(self.message)
+			print(self.items_to_ids)
 		return
+		
+	
+	
 
-logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)	
+	
+	
+
         
 
 	
 if __name__ == "__main__":
+	logging.basicConfig(format='[%(asctime)s] %(message)s', level=logging.INFO)	
+	
 	TEAM = "TeamA"
 	tanks = []
 	for i in range(4):
-		tanks.append(MultiProcessingTank(TEAM+":{}".format(i)))
+		tanks.append(ThreadingTank(TEAM+":{}".format(i)))
 		tanks[-1].start()
+		
 	
 	
     
